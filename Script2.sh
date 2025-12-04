fb657d79ac5e1e939414aaf0556b9e36b093260f
EsTr2007 <estrujilloce@uide.edu.ec>
2025-12-04 13:19:11 +0000

    Mi segundo script

---
+++ Script2.sh
@@ -0,0 +1,8 @@
+# uname se usa para mostrar información del sistema operativo y -a hace que uname muestre toda la información disponible
+uname -a
+# lscpu lista la información del cpu
+lscpu
+# free muestra el uso de memoria
+free -h
+# df muestra el uso del disco
+df -h
