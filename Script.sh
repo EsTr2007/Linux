1fcf792d02fa61faa05a2b3b39bec5c10c71d06d
EsTr2007 <estrujilloce@uide.edu.ec>
2025-12-04 12:56:29 +0000

    Mi primer script

---
+++ Script.sh
@@ -0,0 +1,11 @@
+# Crear y Listar archivos
+# echo muestra un texto en la salida estándar y el símbolo de mayor que redirige la salida estandar a un archivo
+echo "Hola python" > archivo.txt
+# cat uestra el contenido de un archivo en la terminal
+cat archivo.txt
+# cp copia archivos y directorios
+cp archivo.txt copia.txt
+# -l muestre la lista de archivos y directorios con mucha más información
+ls -l 
+# rm elimina archivos o directorios
+rm archivo.txt
